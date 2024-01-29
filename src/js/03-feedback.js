f80948a09b934dad0c159e2cdc395560036de403
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-01-29 16:04:53 +0200

    finish oll part

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+
+const updateFormData = throttle(event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+const handleFormSubmit = event => {
+  event.preventDefault();
+
+  console.log(formData);
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  form.reset();
+};
+
+const populateForm = () => {
+  Object.entries(formData).forEach(([name, value]) => {
+    form.elements[name].value = value;
+  });
+};
+
+form.addEventListener('input', updateFormData);
+form.addEventListener('submit', handleFormSubmit);
+
+document.addEventListener('DOMContentLoaded', populateForm);
