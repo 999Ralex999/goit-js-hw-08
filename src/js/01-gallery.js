e651fa2e898aa66078e49df630ad0338f006acf5
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-01-29 14:16:32 +0200

    3 part

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,56 +1,25 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
 
-/**
-  |============================
-  | Functions
-  |============================
-*/
-const getGalleryItem = ({ preview, original, description }) => {
-  return `<li class="gallery__item">
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"/>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
-  </li>`;
-};
+  </li>`
+  )
+  .join('');
 
-const getGalleryItems = () => {
-  return Array.from(galleryItems, item => getGalleryItem(item)).join('');
-};
+galleryContainer.innerHTML = galleryMarkup;
 
-/**
-  |============================
-  | Initialization
-  |============================
-*/
-galleryContainer.innerHTML = getGalleryItems();
-
-galleryContainer.addEventListener('click', e => {
-  e.preventDefault();
-
-  const imageInstance = basicLightbox.create(
-    `<img src="${e.target.dataset.source}" width="800" height="600">`,
-    {
-      onShow: () => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-
-      onClose: () => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-
-  const onEscKeyPress = e => {
-    if (e.code === 'Escape') {
-      imageInstance.close();
-    }
-  };
-
-  imageInstance.show();
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
