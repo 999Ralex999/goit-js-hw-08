d12b2cf2ff6e2c60c1dea6f2e0ed3abeddd7fb06
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-01-29 15:17:56 +0200

    finish part gallery and video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const videoplayerCurrentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+videoplayerCurrentTime && player.setCurrentTime(videoplayerCurrentTime);
+
+function handlePlaybackTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+player.on('timeupdate', throttle(handlePlaybackTime, 1000));
